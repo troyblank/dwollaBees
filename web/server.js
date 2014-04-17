f90293752ecdd0f05cecf743973057faa0328ffd
troyblank <troy@troyblank.com>
2014-04-17 16:32:14 -0500

    added score to home page

--- web/server.js
+++ web/server.js
@@ -10,6 +10,7 @@ nunjucks.configure(__dirname + '/templates', {
 var server = {
 
     app: express(),
+    page: 'http://dwolla.com',
 
     initialize: function() {
         server.urlConfs();
@@ -21,17 +22,40 @@ var server = {
         console.log('Listening on port 8000');
     },
 
-    // getDaysData: function(date) {
+    getDaysData: function(date, callback) {
+        var path = __dirname + '/../app/data/' + date.getFullYear() + '/' + date.getMonth() + '/' + date.getDate() + '.json';
 
-    // },
+        if (fs.existsSync(path)) {
+            fs.readFile(path, 'utf8', function(err, data) {
+                callback(JSON.parse(data));
+            });
+        } else {
+            return false;
+        }
+    },
 
     //---------------------------------------------------------------------------------------------
     //VIEWS
     //---------------------------------------------------------------------------------------------
     home: function(req, res) {
-        //var today = new Date();
+        //get today or yesterdays data
+        var today = new Date();
+        var data = null;
 
-        res.send(nunjucks.render('home.html'));
+        server.getDaysData(today, function(data) {
+            data = data;
+            if (data) {
+                res.send(nunjucks.render('home.html', data[server.page]));
+            } else {
+                //get yesterdays data
+                today.setDate(today.getDate() - 1);
+                data = server.getDaysData(today, function(data) {
+                    data = data;
+
+                    res.send(nunjucks.render('home.html', data[server.page]));
+                });
+            }
+        });
     },
 
     //---------------------------------------------------------------------------------------------
