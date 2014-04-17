c2cede6cf3c5fab7f1bedffe9e1f589a77c9953f
troyblank <troy@troyblank.com>
2014-04-17 09:15:53 -0500

    added web server and start to front end templating

---
+++ web/server.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var nunjucks = require('nunjucks');
+var fs = require('fs');
+var walk = require('walk');
+
+nunjucks.configure(__dirname + '/templates', {
+    autoescape: false
+});
+
+var server = {
+
+    app: express(),
+
+    initialize: function() {
+        server.urlConfs();
+        server.startWebServer();
+    },
+
+    startWebServer: function() {
+        server.app.listen(8000);
+        console.log('Listening on port 8000');
+    },
+
+    //---------------------------------------------------------------------------------------------
+    //VIEWS
+    //---------------------------------------------------------------------------------------------
+    home: function(req, res) {
+        res.send(nunjucks.render('home.html'));
+    },
+
+    //---------------------------------------------------------------------------------------------
+    //URL CONFS
+    //---------------------------------------------------------------------------------------------
+    urlConfs: function() {
+        server.app.get('/', server.home);
+        //static
+        server.app.use(express.static(__dirname + '/static'));
+    }
+
+
+}
+
+server.initialize();
