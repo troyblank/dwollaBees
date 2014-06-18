2a8197f2b361425a4b14de9faee187985b6190aa
troyblank <troy@troyblank.com>
2014-06-18 16:52:36 -0500

    made dates actual ranges

--- web/static/scripts/control/comparer.js
+++ web/static/scripts/control/comparer.js
@@ -4,18 +4,67 @@ comparer = {
     },
 
     dateRanges: function() {
-        function initialize() {
-            addPikADays();
-            addListeners();
-        }
 
-        function addPikADays() {
-            $('.datepicker').pikaday();
+        var pickerAIn = new Pikaday({
+            field: document.getElementById('a-in-date')
+        });
+        var pickerAOut = new Pikaday({
+            field: document.getElementById('a-out-date')
+        });
+        var pickerBIn = new Pikaday({
+            field: document.getElementById('b-in-date')
+        });
+        var pickerBOut = new Pikaday({
+            field: document.getElementById('b-out-date')
+        });
+
+        function initialize() {
+            addListeners();
+            setPikADayBounds();
         }
 
         function addListeners() {
-
+            $('.datepicker').on('change', inputChangeHand);
         }
+
+        //-----------------------------------------------------------------------------------------
+        //PIKADAY
+        //-----------------------------------------------------------------------------------------
+
+        function setPikADayBounds() {
+            //set A
+            var aIn = pickerAIn.getDate();
+            var aOut = pickerAOut.getDate();
+
+            if (aIn != null) {
+                pickerAOut.setMinDate(new Date(aIn + 1));
+            }
+
+            if (aOut != null) {
+                pickerAIn.setMaxDate(new Date(aOut - 1));
+            }
+
+            //set B
+            var bIn = pickerBIn.getDate();
+            var bOut = pickerBOut.getDate();
+
+            if (bIn != null) {
+                pickerBOut.setMinDate(new Date(bIn + 1));
+            }
+
+            if (bOut != null) {
+                pickerBIn.setMaxDate(new Date(bOut - 1));
+            }
+        }
+
+        //-----------------------------------------------------------------------------------------
+        //HANDLERS
+        //-----------------------------------------------------------------------------------------
+
+        function inputChangeHand() {
+            setPikADayBounds();
+        }
+
 
         initialize();
     }
